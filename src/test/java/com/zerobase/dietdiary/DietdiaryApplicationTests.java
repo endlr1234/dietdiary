1ffa136e1d7a9271b0d0ac4708f2291797d81aab
Doit <endlr12345@naver.com>
2024-03-26 23:46:15 +0900

    기본 설정

---
+++ src/test/java/com/zerobase/dietdiary/DietdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.dietdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietdiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
